e8039ad4a04717b5fff2e936dd0e98ada8a58aa5
munazhairfan <164248882+munazhairfan@users.noreply.github.com>
2024-04-20 18:33:44 +0500

    a simple atm machine

---
+++ index.js
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+let mypin = 2468;
+let mybalance = 10000;
+let pinAnswer = await inquirer.prompt([{
+        name: "pin",
+        type: "number",
+        message: "Enter your pin number"
+    }]);
+if (pinAnswer.pin === mypin) {
+    console.log("correct pin code!!");
+    let operationAnswer = await inquirer.prompt([{
+            name: "operation",
+            type: "list",
+            message: "please select option",
+            choices: ["withdraw", "check balance"],
+        }]);
+    if (operationAnswer.operation === "withdraw") {
+        let amountAnswer = await inquirer.prompt([{
+                name: "amount",
+                type: "number",
+                message: "enter your amount",
+            }]);
+        mybalance -= amountAnswer.amount;
+        console.log("Your remaining balance is " + mybalance);
+    }
+    else if (operationAnswer.operation === "check balance") {
+        console.log("Your balance is " + mybalance);
+    }
+}
+else {
+    console.log("incorrect pin code!!");
+}
+;
